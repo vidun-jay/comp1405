cf934c0bfa26689b0b9e77f2f22085aa0654d478
vidun-jay <69271445+vidun-jay@users.noreply.github.com>
2021-09-21 08:09:30 -0700

    Update comp1405_f21_101224988_assignment_01.py

--- Assignment-1/comp1405_f21_101224988_assignment_01.py
+++ Assignment-1/comp1405_f21_101224988_assignment_01.py
@@ -10,25 +10,36 @@ window = pygame.display.set_mode((480, 640))
 #sets background to white
 window.fill((255,255,255))
 
-#draw blue polygon
-pygame.draw.polygon(window, (79,90,150), [
-    [250, 100],
-    [325, 0],
-    [400, 100],
-    [400, 230],
-    [320,100]], 0)
+#while loop to prevent pygame from instantly closing
+run = True
+while run:
+    #program close definition
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
 
-#draw green polygon
-pygame.draw.polygon(window, (106,155,75), [
-    [320, 430],
-    [400, 430],
-    [400, 530],
-    [300, 690],
-    [150, 690],
-    [150, 530],
-    [320, 530]], 0)
+    #draw blue polygon
+    pygame.draw.polygon(window, (79,90,150), [
+        [250, 100],
+        [325, 0],
+        [400, 100],
+        [400, 230],
+        [320,100]], 0)
 
-#outputs the file to working directory
-pygame.image.save( window, 'assigned_image_for_101224988.png' )
+    #draw green polygon
+    pygame.draw.polygon(window, (106,155,75), [
+        [320, 430],
+        [400, 430],
+        [400, 530],
+        [300, 690],
+        [150, 690],
+        [150, 530],
+        [320, 530]], 0)
 
-pygame.display.update()
+    #outputs the file to working directory
+    pygame.image.save( window, 'assigned_image_for_101224988.png' )
+
+    #closes app after 5 seconds
+    pygame.display.update()
+    pygame.time.wait(1000)
+    pygame.quit()
