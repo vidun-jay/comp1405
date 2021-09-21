257a28f776c3d8b29dc27e9a7a0ca64abbdd2bd8
vidun-jay <vidunvj@gmail.com>
2021-09-21 07:35:39 -0700

    closes instantly now

--- Assignment-1/comp1405_f21_101224988_assignment_01.py
+++ Assignment-1/comp1405_f21_101224988_assignment_01.py
@@ -10,36 +10,25 @@ window = pygame.display.set_mode((480, 640))
 #sets background to white
 window.fill((255,255,255))
 
-#while loop to prevent pygame from instantly closing
-run = True
-while run:
-    #closes the program when 'x' is pressed
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
+#draw blue polygon
+pygame.draw.polygon(window, (79,90,150), [
+    [250, 100],
+    [325, 0],
+    [400, 100],
+    [400, 230],
+    [320,100]], 0)
 
-    #draw blue polygon
-    pygame.draw.polygon(window, (79,90,150), [
-        [250, 100],
-        [325, 0],
-        [400, 100],
-        [400, 230],
-        [320,100]], 0)
+#draw green polygon
+pygame.draw.polygon(window, (106,155,75), [
+    [320, 430],
+    [400, 430],
+    [400, 530],
+    [300, 690],
+    [150, 690],
+    [150, 530],
+    [320, 530]], 0)
 
-    #draw green polygon
-    pygame.draw.polygon(window, (106,155,75), [
-        [320, 430],
-        [400, 430],
-        [400, 530],
-        [300, 690],
-        [150, 690],
-        [150, 530],
-        [320, 530]], 0)
+#outputs the file to working directory
+pygame.image.save( window, 'assigned_image_for_101224988.png' )
 
-    #outputs the file to working directory
-    pygame.image.save( window, 'assigned_image_for_101224988.png' )
-
-    #closes app after 5 seconds
-    pygame.display.update()
-    pygame.time.wait(5000)
-    pygame.quit()
+pygame.display.update()
