e14f89485341814e72e25510451cea3a0101e908
vidun-jay <vidunvj@gmail.com>
2021-09-22 21:52:10 -0700

    Update comp1405_f21_101224988_tutorial_01_a.py

---
+++ tutorial-1/comp1405_f21_101224988_tutorial_01_a.py
@@ -0,0 +1,37 @@
+#Vishva Vidun Jayakody, 101224988
+import pygame
+import math
+
+pi = math.pi
+# three_pi = (3 * math.pi())/2
+
+#initialize pygame
+pygame.init()
+
+#sets display dimensions
+window = pygame.display.set_mode((600, 600))
+
+#sets background to white
+window.fill((170,76,49))
+
+while True:
+    #program close definition
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    #blue segment
+    pygame.draw.arc(window, (67,129,183), [150,150,300,300], ((3*pi)/2), (pi/2)+0.01, 50)
+    #white segment
+    pygame.draw.arc(window, (221,212,200), [150,150,300,300], pi/2, ((3*pi)/2)+0.01, 50)
+    #yellow segment
+    pygame.draw.arc(window, (227,180,85), [200,200,200,200], ((3*pi)/2), (pi/2)+0.009, 50)
+    #pink segment
+    pygame.draw.arc(window, (227,166,151), [250,250,100,100], ((3*pi)/2), (pi/2)+0.009, 50)
+    #black segment
+    pygame.draw.arc(window, (30,30,30), [200,200,200,200], pi/2, ((3*pi)/2)+0.01, 100)
+
+    #outputs the file to working directory
+    pygame.image.save( window, 'assigned_image_for_101224988.png')
+
+    pygame.display.update()
