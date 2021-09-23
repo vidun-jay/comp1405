34b48c6feda6c3d412c9f00838d3013cd612167a
vidun-jay <vidunvj@gmail.com>
2021-09-23 07:26:17 -0700

    Fixed typos in comments

--- tutorial-1/comp1405_f21_101224988_tutorial_01_a.py
+++ tutorial-1/comp1405_f21_101224988_tutorial_01_a.py
@@ -11,7 +11,7 @@ pygame.init()
 #sets display dimensions
 window = pygame.display.set_mode((600, 600))
 
-#sets background to white
+#sets background to red
 window.fill((170,76,49))
 
 while True:
@@ -21,14 +21,14 @@ while True:
             exit()
 
     #blue segment
-    pygame.draw.arc(window, (67,129,183), [150,150,300,300], ((3*pi)/2), (pi/2)+0.01, 50)
+    pygame.draw.arc(window, (67,129,183), [150,150,300,300], (3*pi)/2, (pi/2)+0.01, 50)
     #white segment
-    pygame.draw.arc(window, (221,212,200), [150,150,300,300], pi/2, ((3*pi)/2)+0.01, 50)
+    pygame.draw.arc(window, (221,212,200), [150,150,300,300], pi/2, (3*pi/2)+0.01, 50)
     #yellow segment
-    pygame.draw.arc(window, (227,180,85), [200,200,200,200], ((3*pi)/2), (pi/2)+0.009, 50)
+    pygame.draw.arc(window, (227,180,85), [200,200,200,200], (3*pi)/2, (pi/2)+0.009, 50)
     #pink segment
-    pygame.draw.arc(window, (227,166,151), [250,250,100,100], ((3*pi)/2), (pi/2)+0.009, 50)
+    pygame.draw.arc(window, (227,166,151), [250,250,100,100], (3*pi)/2, (pi/2)+0.009, 50)
     #black segment
-    pygame.draw.arc(window, (30,30,30), [200,200,200,200], pi/2, ((3*pi)/2)+0.01, 100)
+    pygame.draw.arc(window, (30,30,30), [200,200,200,200], pi/2, (3*pi/2)+0.01, 100)
 
     pygame.display.update()
