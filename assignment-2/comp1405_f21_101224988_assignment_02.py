db0bcd13e6c8af3309b3d5c45f025dfb4f27a9dc
vidun-jay <vidunvj@gmail.com>
2021-09-28 07:11:58 -0700

    made code more verbose

--- assignment-2/comp1405_f21_101224988_assignment_02.py
+++ assignment-2/comp1405_f21_101224988_assignment_02.py
@@ -15,22 +15,28 @@ cmd_line_arg = int(sys.argv[1])
 
 #divide by 3 and round result down
 running_total = cmd_line_arg // 3
+print(running_total, " - (divide by 3 and round down)")
 
 #add it to the integer that is one more than itself
 running_total = running_total + (cmd_line_arg + 1)
+print(running_total, " - (add to integer 1 more than itself)")
 
 #subtract 3
 running_total -= 3
+print(running_total, " - (subtract 3)")
 
 #add the return value from input function call
 running_total += input
+print(running_total, " - (add to input function call)")
 
 #multiply it by 7.181918
 running_total = running_total * 7.181918
+print(running_total, " - (multiply by 7.181918)")
 
 #convert it to an integer
 running_total = int(running_total)
+print(running_total, " - (convert to int)")
 
 #convert it to a character
 running_total = chr(running_total)
-print(running_total)
+print(running_total, " - (convert to char)")
