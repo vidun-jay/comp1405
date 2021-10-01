b3bf96cad23b8e3f3b308efaaa7ecc74c2f6613c
vidun-jay <vidunvj@gmail.com>
2021-10-01 09:09:03 -0700

    Semantics

--- tutorial-2/comp1405_f21_101224988_tutorial_02_a.py
+++ tutorial-2/comp1405_f21_101224988_tutorial_02_a.py
@@ -8,7 +8,7 @@ birth_year = int(input("In what year were you born? "))
 #calculate age
 age1 = current_year - birth_year
 age2 = (current_year - birth_year) - 1
-leap = round((((current_year - birth_year) // 4)), 1)
+leap = (current_year - birth_year) // 4
 
 #print age
-print("Hello", name + ", you are either", age2, "or", str(age1) + ". Unless you were born on a leap year, in which case you'd be", leap, "years old.")
+print("Hello", name + ", you are either", age2, "or", str(age1) + ". Unless you were actually born on a leap day, in which case you'd be", leap, "years old.")
