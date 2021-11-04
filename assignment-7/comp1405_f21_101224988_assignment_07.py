1da8f4e6fc354c5b61b7dac2675cfdf22d47b109
vidun-jay <vidunvj@gmail.com>
2021-11-04 13:31:00 -0700

    Uploading assignment 7

--- assignment-7/comp1405_f21_101224988_assignment_07.py
+++ assignment-7/comp1405_f21_101224988_assignment_07.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 
 #ask user for image path
-img_path = '/Users/vidunjayakody/Desktop/comp1405/assignment-7/image.jpg'
+img_path = input('Enter the path of an image: ')
 img = pygame.image.load_extended(img_path)
 
 #get dimensions of image and create window with that size
@@ -21,40 +21,43 @@ pygame.display.update()
 exit_flag = False
 while not exit_flag:
 
+    #check if the (left) mouse button has been pressed
     mouse_press = pygame.mouse.get_pressed()
-
     if mouse_press[0]:
-
-        mouse_pos = pygame.mouse.get_pos()   
-        print(mouse_pos) ### FOR TESTING, REMEMBER TO DELETE ###
         
+        #clock for fps to slow the loop down
+        clock = pygame.time.Clock()
+        mouse_pos = pygame.mouse.get_pos() #get position of mouse 
+        
+        #if the full rectangle can be drawn
         if mouse_pos[0] >= 200 and mouse_pos[1] >= 100:
             area_x = mouse_pos[0] - 200
             area_y = mouse_pos[1] - 100
-
+        #if it's less than the width of the rect, use 0 instead of mouse_pos (can't have -ve pixels)
         elif mouse_pos[0] < 200:
             area_x = 0
             area_y = mouse_pos[1] - 100
-
         elif mouse_pos[1] < 100:
             area_x = mouse_pos[0] - 200
             area_y = 0
-
+        #top left case, draw rectangle from (0, 0) (also because can't have -ve pixels)
         elif mouse_pos[0] < 200 and mouse_pos[1] < 100:
             mouse_pos[0] = 0
             mouse_pos[1] = 0
             area_x = 0
             area_y = 0
         
+        #'draw' the rectangle
         for i in range(area_y, mouse_pos[1]):
             for j in range(area_x, mouse_pos[0]):
+                #handling index out of bounds error, can't have -ve pixels
                 if i >= 0 and j >= 0:
                     rgb = window.get_at((j, i))
                 else:
                     rgb = window.get_at((0, 0))
-                
                 window.set_at((j,i), (255-rgb[0], 255-rgb[1], 255-rgb[2]))
-        pygame.display.update()
+        pygame.display.update() #update display
+        clock.tick(4) #slowing down the loop
 
     for e in pygame.event.get(): 
         if e.type == pygame.QUIT:
