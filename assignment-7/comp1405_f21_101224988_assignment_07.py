163a66ca5a2b60be18544a2a977e708ceee22e95
vidun-jay <vidunvj@gmail.com>
2021-11-03 20:52:10 -0700

    more testing

--- assignment-7/comp1405_f21_101224988_assignment_07.py
+++ assignment-7/comp1405_f21_101224988_assignment_07.py
@@ -11,6 +11,7 @@ img = pygame.image.load_extended(img_path)
 #get dimensions of image and create window with that size
 img_dim = img.get_size()
 window = pygame.display.set_mode(img_dim)
+pygame.display.set_caption('Color Inverter')
 
 #draw image on window
 window.blit(img, (0,0))
@@ -21,17 +22,40 @@ exit_flag = False
 while not exit_flag:
 
     mouse_press = pygame.mouse.get_pressed()
-    mouse_pos = pygame.mouse.get_pos()
-    area_x = mouse_pos[0] - 100
-    area_y = mouse_pos[1] - 60    
 
     if mouse_press[0]:
+
+        mouse_pos = pygame.mouse.get_pos()   
+        print(mouse_pos) ### FOR TESTING, REMEMBER TO DELETE ###
+        
+        if mouse_pos[0] >= 200 and mouse_pos[1] >= 100:
+            area_x = mouse_pos[0] - 200
+            area_y = mouse_pos[1] - 100
+
+        elif mouse_pos[0] < 200:
+            area_x = 0
+            area_y = mouse_pos[1] - 100
+
+        elif mouse_pos[1] < 100:
+            area_x = mouse_pos[0] - 200
+            area_y = 0
+
+        elif mouse_pos[0] < 200 and mouse_pos[1] < 100:
+            mouse_pos[0] = 0
+            mouse_pos[1] = 0
+            area_x = 0
+            area_y = 0
+        
         for i in range(area_y, mouse_pos[1]):
             for j in range(area_x, mouse_pos[0]):
-                rgb = window.get_at((j, i))
+                if i >= 0 and j >= 0:
+                    rgb = window.get_at((j, i))
+                else:
+                    rgb = window.get_at((0, 0))
+                
                 window.set_at((j,i), (255-rgb[0], 255-rgb[1], 255-rgb[2]))
         pygame.display.update()
-    
+
     for e in pygame.event.get(): 
         if e.type == pygame.QUIT:
           exit_flag = True
