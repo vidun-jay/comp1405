b1efb8a6a51bea9b689c8c57edec5615b87291f8
vidun-jay <69271445+vidun-jay@users.noreply.github.com>
2021-11-28 10:39:16 -0800

    Added comments

--- tutorial-8/comp1405_f21_101224988_tutorial_08_a.py
+++ tutorial-8/comp1405_f21_101224988_tutorial_08_a.py
@@ -1,20 +1,27 @@
 #Vishva Vidun Jayakody, 101224988
 import random
 
+#function that takes in a length and returns a list of random items of that length seperated by ","
 def rowGenerator(length):
     row = [str(random.randint(0,4)) for i in range(length)]
     return ','.join(row)
 
 def main():
-    tile_map = []
+
+    #create configuration file
     tile_map_txt = open("tiles.conf", "w")
 
+    #prompt the user for the prefix
     prefix = input("Tiles: ")
     
+    #make the first line the prefix
     tile_map_txt.write("tiles: " + prefix + "\n\n")
 
+    #make a new row for 8 columns (creating the 6x8 grid)
     for i in range (8):
         tile_map_txt.write((str(rowGenerator(6)) + ",\n"))
+    
+    #inform user of success creating config file
     print("Configuration file created!")
     tile_map_txt.close()
 
